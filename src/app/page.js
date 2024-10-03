424a3ceb4f901939ecafd8b20ce0cdfc8697ebe2
Fabricio Jorge <fabricioj.dev@gmail.com>
2024-10-03 12:08:35 -0400

    finalizando header e iniciando main

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Topo from '@/Components/Topo'
+import Categoria_buscas from "@/Components/Categoria_buscas";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Topo/>
       <main>
-        
+        <Categoria_buscas/>
       </main>
     </div>
   );
